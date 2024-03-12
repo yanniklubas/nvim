850c7ec5c66de8de354c0f6d26f5c5378f6aa65e
Yannik Lubas <yannik.lubas@gmail.com>
2024-03-12 10:45:13 +0100

    FEAT: Initial configuration

--- lua/yannik/plugins/obsidian.lua
+++ lua/yannik/plugins/obsidian.lua
@@ -29,6 +29,88 @@ return {
 		"BufReadPre " .. vim.fn.expand("~") .. "work/notes/**.md",
 		"BufReadPre " .. vim.fn.expand("~") .. "personal/notes/**.md",
 	},
+	keys = {
+		{
+			"<leader>oo",
+			"<CMD>ObsidianOpen<CR>",
+			mode = "n",
+			desc = "[O]pen the current in the [O]bsidian App",
+		},
+		{
+			"<leader>on",
+			"<CMD>ObsidianNew<CR>",
+			mode = "n",
+			desc = "Create a [N]ew [O]bsidian Note",
+		},
+		{
+			"<leader>oq",
+			"<CMD>ObsidianQuickSwitch<CR>",
+			mode = "n",
+			desc = "[Q]uickly switch to (or open) another note",
+		},
+		{
+			"<leader>os",
+			"<CMD>ObsidianSearch<CR>",
+			mode = { "n", "v" },
+			desc = "[S]earch (or create) another note",
+		},
+		{
+			"<leader>oft",
+			"<CMD>ObsidianTags<CR>",
+			mode = "n",
+			desc = "[F]ind notes by [T]ag",
+		},
+		{
+			"<leader>oy",
+			"<CMD>ObsidianToday -1<CR>",
+			mode = "n",
+			desc = "Open (or create) the daily note for [Y]esterday",
+		},
+		{
+			"<leader>od",
+			"<CMD>ObsidianToday<CR>",
+			mode = "n",
+			desc = "Open (or create) the [D]aily note for today",
+		},
+		{
+			"<leader>ot",
+			"<CMD>ObsidianToday 1<CR>",
+			mode = "n",
+			desc = "Open (or create) the daily note for [T]omorrow",
+		},
+		{
+			"<leader>oc",
+			function()
+				require("obsidian").util.toggle_checkbox()
+			end,
+			mode = "n",
+			desc = "Toggle the [C]heckbox in an [O]bsidian note",
+		},
+		{
+			"<leader>ole",
+			"<CMD>ObsidianLink<CR>",
+			mode = "v",
+			desc = "[L]ink selected text to an [E]xisting note",
+		},
+		{
+			"<leader>oln",
+			"<CMD>ObsidianLinkNew<CR>",
+			mode = "v",
+			desc = "[L]ink selected text to a [N]ew note",
+		},
+		{
+			"<leader>ol",
+			"<CMD>ObsidianFollowLink vsplit<CR>",
+			mode = "n",
+			desc = "Follow [L]ink under cursor in a vsplit",
+		},
+		{
+			"<leader>oi",
+			"<CMD>ObsidianTemplate<CR>",
+			mode = "n",
+			desc = "[I]nsert an [O]bsidian Template",
+		},
+	},
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"hrsh7th/nvim-cmp",
@@ -36,101 +118,104 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	},
 	opts = {
-		workspaces = {
-			{
-				name = "personal",
-				path = "~/personal/notes/",
-			},
-			{
-				name = "work",
-				path = "~/work/notes/",
-			},
-		},
-		daily_notes = {
-			folder = "dailies",
-			date_format = "%Y-%m-%d-%a-%b-%d",
-			template = "daily_template.md",
-		},
-		completion = {
-			nvim_cmp = true,
-			min_chars = 2,
-		},
-		mappings = {
-			["gf"] = {
-				action = function()
-					return require("obsidian").util.gf_passthrough()
-				end,
-				opts = { noremap = false, expr = true, buffer = true },
-			},
-			["<leader>oc"] = {
-				action = function()
-					return require("obsidian").util.toggle_checkbox()
-				end,
-				opts = { buffer = true, desc = "[O]bsidian toggle [C]heckbox" },
-			},
-		},
-		follow_url_func = function(url)
-			vim.fn.jobstart({ "xdg-open", url })
-		end,
-		templates = {
-			subdir = "templates",
-			date_format = "%Y-%m-%d-%a-%b-%d",
-			time_format = "%H-%M",
-			substitutions = {
-				alias_date = function()
-					return os.date("%a %b %d %Y", os.time())
-				end,
-				title_date = function()
-					return os.date("%a %b %d", os.time())
-				end,
-				yesterday = function()
-					local day_in_seconds = 86400
-					return os.date("%a-%b-%d-%Y", os.time() - day_in_seconds)
-				end,
-				tomorrow = function()
-					local day_in_seconds = 86400
-					return os.date("%a-%b-%d-%Y", os.time() + day_in_seconds)
-				end,
-				week = function()
-					return os.date("%Y, Week %W", os.time())
-				end,
-				sunday = function()
-					return getWeekDayOfCurrentWeek(0)
-				end,
-				monday = function()
-					return getWeekDayOfCurrentWeek(2)
-				end,
-				tuesday = function()
-					return getWeekDayOfCurrentWeek(3)
-				end,
-				wednesday = function()
-					return getWeekDayOfCurrentWeek(4)
-				end,
-				thursday = function()
-					return getWeekDayOfCurrentWeek(5)
-				end,
-				friday = function()
-					return getWeekDayOfCurrentWeek(6)
-				end,
-				saturday = function()
-					return getWeekDayOfCurrentWeek(7)
-				end,
-			},
-		},
-		picker = {
-			-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-			name = "telescope.nvim",
-			-- Optional, configure key mappings for the picker. These are the defaults.
-			-- Not all pickers support all mappings.
-			mappings = {
-				-- Create a new note from your query.
-				new = "<C-x>",
-				-- Insert a link to the selected note.
-				insert_link = "<C-l>",
-			},
-		},
+		"some",
 	},
-	init = function()
+	config = function()
+		require("obsidian").setup({
+			workspaces = {
+				{
+					name = "personal",
+					path = "~/personal/notes/",
+				},
+				{
+					name = "work",
+					path = "~/work/notes/",
+				},
+			},
+			daily_notes = {
+				folder = "dailies",
+				date_format = "%Y-%m-%d-%a-%b-%d",
+				template = "daily_template.md",
+			},
+			completion = {
+				nvim_cmp = true,
+				min_chars = 2,
+			},
+			mappings = {
+				["gf"] = {
+					action = function()
+						return require("obsidian").util.gf_passthrough()
+					end,
+					opts = { noremap = false, expr = true, buffer = true },
+				},
+				["<leader>oc"] = {
+					action = function()
+						return require("obsidian").util.toggle_checkbox()
+					end,
+					opts = { buffer = true, desc = "[O]bsidian toggle [C]heckbox" },
+				},
+			},
+			follow_url_func = function(url)
+				vim.fn.jobstart({ "xdg-open", url })
+			end,
+			templates = {
+				subdir = "templates",
+				date_format = "%Y-%m-%d-%a-%b-%d",
+				time_format = "%H-%M",
+				substitutions = {
+					alias_date = function()
+						return os.date("%a %b %d %Y", os.time())
+					end,
+					title_date = function()
+						return os.date("%a %b %d", os.time())
+					end,
+					yesterday = function()
+						local day_in_seconds = 86400
+						return os.date("%a-%b-%d-%Y", os.time() - day_in_seconds)
+					end,
+					tomorrow = function()
+						local day_in_seconds = 86400
+						return os.date("%a-%b-%d-%Y", os.time() + day_in_seconds)
+					end,
+					week = function()
+						return os.date("%Y, Week %W", os.time())
+					end,
+					sunday = function()
+						return getWeekDayOfCurrentWeek(0)
+					end,
+					monday = function()
+						return getWeekDayOfCurrentWeek(2)
+					end,
+					tuesday = function()
+						return getWeekDayOfCurrentWeek(3)
+					end,
+					wednesday = function()
+						return getWeekDayOfCurrentWeek(4)
+					end,
+					thursday = function()
+						return getWeekDayOfCurrentWeek(5)
+					end,
+					friday = function()
+						return getWeekDayOfCurrentWeek(6)
+					end,
+					saturday = function()
+						return getWeekDayOfCurrentWeek(7)
+					end,
+				},
+			},
+			picker = {
+				-- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+				name = "telescope.nvim",
+				-- Optional, configure key mappings for the picker. These are the defaults.
+				-- Not all pickers support all mappings.
+				mappings = {
+					-- Create a new note from your query.
+					new = "<C-x>",
+					-- Insert a link to the selected note.
+					insert_link = "<C-l>",
+				},
+			},
+		})
 		vim.api.nvim_create_user_command("ObsidianWeekly", function()
 			local client = require("obsidian").get_client()
 			local templates = require("obsidian.templates")
--- lua/yannik/plugins/which-key.lua
+++ lua/yannik/plugins/which-key.lua
@@ -1,9 +1,12 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 500
-    end,
-    opts = {},
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+		require("which-key").register({
+			["<leader>o"] = { name = "+Obsidian" },
+		}, {})
+	end,
+	opts = {},
 }
