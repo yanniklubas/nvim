56d1c77d95cdc2e8c1df2eccfe292d55b79c312c
Yannik Lubas <125293446+yanniklubas@users.noreply.github.com>
2025-02-07 19:56:03 +0100

    chore: remove deprecated method

--- lua/yannik/plugins/which-key.lua
+++ lua/yannik/plugins/which-key.lua
@@ -4,9 +4,6 @@ return {
 	init = function()
 		vim.o.timeout = true
 		vim.o.timeoutlen = 500
-		require("which-key").register({
-			["<leader>o"] = { name = "+Obsidian" },
-		}, {})
 	end,
 	opts = {},
 }
