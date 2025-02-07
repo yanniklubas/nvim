f1d00047164f0be15a76875a6c51bf42ef699114
Yannik Lubas <125293446+yanniklubas@users.noreply.github.com>
2025-02-07 20:11:00 +0100

    feat: update select behavior

--- lua/yannik/plugins/lsp/nvim-cmp.lua
+++ lua/yannik/plugins/lsp/nvim-cmp.lua
@@ -32,7 +32,7 @@ return {
 			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-p>"] = cmp.mapping.select_prev_item(),
-				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
